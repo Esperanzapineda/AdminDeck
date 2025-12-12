436b2cdf627aaa79844538a00056690c76c21125
esperanza <judithesperanza1983@gmail.com>
2025-12-11 21:48:52 -0500

    feat: add inicio dashboard

---
+++ front/src/app/(views)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+import { getDashboardStats } from "@/services/dashboard.services";
+import DashboardView from "./components/dashboard-view";
+
+export default async function DashboardPage() {
+  const stats = await getDashboardStats();
+  
+  return <DashboardView stats={stats} />;
+}
